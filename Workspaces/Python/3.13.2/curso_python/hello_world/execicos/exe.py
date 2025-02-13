5aaf5c2383b06be4a0e14966ae9cf8e21b70d669
Rafael Paiva <rafaelpaiva062@gmail.com>
2025-02-13 17:30:35 -0300

    new

--- Workspaces/Python/3.13.2/curso_python/hello_world/execicos/exe.py
+++ Workspaces/Python/3.13.2/curso_python/hello_world/execicos/exe.py
@@ -3,28 +3,61 @@
 # soma = 0 
 
 # while True:
-#     num1= int(input('Digite  primeiro  número:\n'))
-#     num2= int(input("Digite o segundo número: \n"))
-    
-#     print(" 1- Suma\n 2- subtração\n 3- divisão \n 4- multiplicação \n 5- Sair: ")
-#     cacular=int(input("digite que tipo de operação matemática:\n"))
-#     match cacular:
-#         case 1:
-#              print(f" {num1} + {num2} = {num1+ num2}")
-#         case 2:
-#              print(f" {num1} + {num2} = {num1- num2}")
-#         case 3:
-#              print(f" {num1} + {num2} = {num1/ num2}")
-#         case 4:
-#              print(f" {num1} + {num2} = {num1* num2}")
-#         case 5:
-#              print("Bye,bye ;;;;")
-#              break
-#         case _:
-#              print("Tentado"*30)
-#              ...
-def verificação_sala():
-    while True:
-        print("$$$$$$$"*30)
-        print("Sejá bem vindo ao nosso centrau de filmes com 5 opições:")
-        print("%+$$"*30)
+#     try:
+#         num1= int(input('Digite  primeiro  número:\n'))
+#         num2= int(input("Digite o segundo número: \n"))
+        
+#         print(" 1- Suma\n 2- subtração\n 3- divisão \n 4- multiplicação \n 5- Sair: ")
+#         cacular=int(input("digite que tipo de operação matemática:\n"))
+#         match cacular:
+#             case 1:
+#                 print(f" {num1} + {num2} = {num1+ num2}")
+#             case 2:
+#                 print(f" {num1} + {num2} = {num1- num2}")
+#             case 3:
+#                 print(f" {num1} + {num2} = {num1/ num2:.2f}")
+#             case 4:
+#                 print(f" {num1} + {num2} = {num1* num2}")
+#             case 5:
+#                 print("Bye,bye ;;;;")
+#                 break
+#             case _:
+#                 print("Tentado"*30)
+#                 ...
+#     except ValueError:
+#          print("O programa não fucionol")
+
+# execicio B
+
+# sala =0
+# idade=0
+# def verificação_sala(sala, idade): 
+
+#     while True:
+#         print("$$$$$$$"*30)
+#         print("Sejá bem vindo ao nosso centrau de filmes com 5 opições:\n", end="")
+#         print("$$$$$$$"*30)
+#         sala=int(input(" Salas:\n 1- Menina De Ouro\n 2-Psicose\n 3-Bastardos Inglórios\n 4-Parasita\n 5-Cidade De Deus\n Insirar sua opção:\n"))
+#         idade = int(input("Digite sua idade: \n"))
+#         print("$$$$$$$"*30)
+#         if(sala ==1 ):
+#             print("*************\n  INGRESSO DE CINEMA \n**************\n.")
+#             break
+#         elif(sala == 2 and idade>=15):
+#             print("*************\n  INGRESSO DE CINEMA \n**************\n.")
+#             break
+#         elif(sala == 3 and  idade>=18):
+#             print("*************\n  INGRESSO DE CINEMA \n**************\n.")
+#             break
+#         elif(sala ==4 ):
+#             print("*************\n  INGRESSO DE CINEMA \n**************\n.")
+#             break
+#         elif(sala ==5 and idade >=12 ):
+#             print("*************\n  INGRESSO DE CINEMA \n**************\n.")
+#             break
+#         else:
+#             print("Sala não encontrada ou idade não permitida.")
+#             continue
+        
+# verificação_sala(sala, idade)
+
